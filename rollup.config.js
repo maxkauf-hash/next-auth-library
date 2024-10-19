1ce0872e40c6ecb3a0843cf9140b8f0a4e440292
maxkauf-hash <77574291+maxkauf-hash@users.noreply.github.com>
2024-10-19 19:54:46 +0200

    feat: Setup Rollup config

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import babel from "rollup-plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+      },
+      {
+        file: "dist/index.esm.js",
+        format: "esm",
+        exports: "named",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
+      external(),
+      resolve(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+      }),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-react"],
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+      }),
+    ],
+  },
+];

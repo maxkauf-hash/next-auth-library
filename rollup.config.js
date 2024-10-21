a2fcf987f6df16c3ddf97457ccd695579ad8e2bb
maxkauf-hash <77574291+maxkauf-hash@users.noreply.github.com>
2024-10-21 15:45:19 +0200

    feat: fix terser import module import error

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default [
   {

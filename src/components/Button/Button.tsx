ba11e88e2bd711ac33d9b75301fa439a66a2c792
maxkauf-hash <77574291+maxkauf-hash@users.noreply.github.com>
2024-10-23 08:39:45 +0200

    feat: update Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,40 +1,4 @@
 import * as React from "react";
-import { cva } from "class-variance-authority";
-
-import { cn } from "../../utils";
-
-/**
- * Configuration des variantes de styles pour le bouton.
- * Utilise `cva` pour générer des classes en fonction des propriétés `variant`, `size`, et `rounded`.
- */
-export const buttonVariants = cva("m-2 p-2 max-w-fit", {
-  variants: {
-    variant: {
-      default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-      destructive:
-        "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-      outline:
-        "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-      secondary:
-        "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-      ghost: "hover:bg-accent hover:text-accent-foreground",
-      link: "text-primary underline-offset-4 hover:underline",
-    },
-    size: {
-      default: "h-9 px-4 py-2",
-      sm: "h-8 rounded-md px-3 text-xs",
-      lg: "h-10 rounded-md px-8",
-      icon: "h-9 w-9",
-    },
-    rounded: {
-      sm: "rounded-sm",
-      md: "rounded-md",
-      lg: "rounded-lg",
-      none: "rounded-none",
-      full: "rounded-full",
-    },
-  },
-});
 
 /**
  * Interface représentant les propriétés d'un composant Button.
@@ -103,7 +67,7 @@ export const Button = ({
 }: ButtonProps): JSX.Element => {
   return (
     <button
-      className={`${buttonVariants({ variant, size, rounded })}`}
+      className={`m-2 p-2 max-w-fit bg-${variant} text-${size} rounded-${rounded} ${className}`}
       {...props}
     >
       {children}
@@ -111,4 +75,4 @@ export const Button = ({
   );
 };
 
-export default { Button, buttonVariants };
+export default { Button };

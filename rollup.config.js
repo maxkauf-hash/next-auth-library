c44205cc60aa324a15dc593c3e4f0f064df11551
maxkauf-hash <77574291+maxkauf-hash@users.noreply.github.com>
2024-10-27 10:45:28 +0100

    feat: add postinstall and load-env scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
+import { terser } from "@rollup/plugin-terser";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 
